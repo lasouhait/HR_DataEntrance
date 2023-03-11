33a31aca1e8adc6993a86c4ab79e93d1c3fa091f
lasouhait <daniel40307@yahoo.com.tw>
2023-03-11 20:03:30 +0800

    Updated

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const root = document.body;
-if (localStorage.getItem("color-palette")) {
-  var palette = localStorage.getItem("color-palette");
-  root.setAttribute("color-palette",palette);
+if (localStorage.getItem("color-text")) {
+  var color_text = localStorage.getItem("color-text");
+  root.setAttribute("color-text",color_text);
 }
-var buttons = document.querySelectorAll("button[color-palette]");
+if (localStorage.getItem("color-bg")) {
+  var color_bg = localStorage.getItem("color-bg");
+  root.setAttribute("color-bg",color_bg);
+}
+var buttons = document.querySelectorAll("button[color-text]");
 buttons.forEach(function (button) {
   button.addEventListener("click", function () {
-    var attr = this.getAttribute("color-palette");
-    document.body.setAttribute("color-palette", attr);
-    localStorage.setItem("color-palette", attr);
+    var attr = this.getAttribute("color-text");
+    document.body.setAttribute("color-text", attr);
+    localStorage.setItem("color-text", attr);
+  });
+  button.style.setProperty(
+    "color",
+    button.getAttribute("color-text")
+  );
+});
+var buttons = document.querySelectorAll("button[color-bg]");
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    var attr = this.getAttribute("color-bg");
+    document.body.setAttribute("color-bg", attr);
+    localStorage.setItem("color-bg", attr);
   });
   button.style.setProperty(
     "background-color",
-    button.getAttribute("color-palette")
+    button.getAttribute("color-bg")
   );
 });
